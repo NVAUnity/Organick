d131dd327e95e436bdb8631df23d955243b80028
NVAUnity <108986997+NVAUnity@users.noreply.github.com>
2022-07-29 14:19:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Nav Icon
+
+const navBtn = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+navBtn.onclick = function() {
+    nav.classList.toggle('nav--mobile');
+    menuIcon.classList.toggle('menu-icon-active');
+};
